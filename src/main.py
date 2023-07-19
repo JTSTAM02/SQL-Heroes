931cb0bb372a7c2bc6eb4b46682df63a8398ce9e
Tyler Stamm <121567944+JTSTAM02@users.noreply.github.com>
2023-07-19 20:44:46 +0000

    Add hero without superpower

--- src/main.py
+++ src/main.py
@@ -1,3 +1,17 @@
+import psycopg2
 from database.connection import execute_query, create_connection
 
 execute_query("SELECT * FROM Heroes;")
+
+def create_hero():
+    # Prompt the user for hero details
+    name = input("Enter the hero's name: ")
+    about_me = input("Enter a short description about the hero: ")
+    biography = input("Enter the hero's biography: ")
+    query = "INSERT INTO heroes (name, about_me, biography) VALUES (%s, %s, %s)"
+    params = (name, about_me, biography)
+    execute_query(query, params)
+    print("Hero created successfully!")
+
+
+execute_query(create_hero())
