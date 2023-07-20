dbca9429a8ccc96f2ec89187107c8d9a65b09ab7
Tyler Stamm <121567944+JTSTAM02@users.noreply.github.com>
2023-07-20 16:04:52 -0400

    Update connection.py

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,4 +1,37 @@
-from psycopg2 import connect, OperationalError
+# from psycopg2 import connect, OperationalError
+
+# def create_connection(db_name, db_user, db_password, db_host="localhost", db_port="5432"):
+#     connection = None
+#     try:
+#         connection = connect(
+#             dbname=db_name,
+#             user=db_user,
+#             password=db_password,
+#             host=db_host,
+#             port=db_port,
+#         )
+#         print("Connection to PostgreSQL DB successful")
+#     except OperationalError as e:
+#         print(f"The error '{e}' occurred")
+#     return connection
+
+# def execute_query(query, params=None):
+#     connection = create_connection("postgres", "postgres", "postgres")
+#     cursor = connection.cursor()
+#     try:
+#         if params is not None:
+#             cursor.execute(query, params)
+#         else:
+#             cursor.execute(query)
+#         connection.commit()
+#         print("Query executed successfully")
+#         return cursor.fetchall(), True
+#     except Exception as e:
+#         print(f"Error: {e}")
+#         connection.close()
+#         return None, False
+
+from psycopg2 import connect, OperationalError, errors
 
 def create_connection(db_name, db_user, db_password, db_host="localhost", db_port="5432"):
     connection = None
@@ -23,10 +56,22 @@ def execute_query(query, params=None):
             cursor.execute(query, params)
         else:
             cursor.execute(query)
+
+        if cursor.description is not None: 
+            result = cursor.fetchall()
+        else:
+            result = None
+
         connection.commit()
         print("Query executed successfully")
-        return cursor.fetchall(), True
+        connection.close()
+        return result, True 
+    except errors.UndefinedTable:
+        print("There is not a table with that name. Please check your spelling and try again.")
+        connection.close()
+        return None, False  
     except Exception as e:
         print(f"Error: {e}")
         connection.close()
-        return None, False
+        return None, False 
+    
