6729dbe0869c4be1b3eec4ba455f01138ca37f66
Tyler Stamm <121567944+JTSTAM02@users.noreply.github.com>
2023-07-20 16:05:51 -0400

    Update delete_hero.py

--- src/delete_hero.py
+++ src/delete_hero.py
@@ -1,17 +1,42 @@
+# from database.connection import execute_query
+
+# def delete_hero(hero_name):
+
+#     query = "SELECT id FROM heroes WHERE LOWER(name) = LOWER(%s)"
+#     params = (hero_name, ) # comma needed to convert to a tuple
+#     rows, success = execute_query(query, params)
+#     if not success:
+#         print("Error occurred while looking up the hero.")
+#         return
+    
+#     if rows:
+#         hero_id = rows[0][0]
+#         delete_query = "DELETE FROM Heroes WHERE id = %s"
+#         delete_params = (hero_id, )
+#         execute_query(delete_query, delete_params)
+#         print(f"Hero with ID {hero_id} deleted successfully.")
+#     else:
+#         print(f"Hero '{hero_name}' not found.")
+
+# hero_name_input = input("Type in which hero you would like to delete: ")
+# execute_query(delete_hero(hero_name_input))
+
 from database.connection import execute_query
 
 def delete_hero(hero_name):
-
     query = "SELECT id FROM heroes WHERE LOWER(name) = LOWER(%s)"
-    params = (hero_name, ) # comma needed to convert to a tuple
-    rows, success = execute_query(query, params)
-    if not success:
+    params = (hero_name, )  # comma needed to convert to a tuple
+    result = execute_query(query, params)
+
+    if not result[1]:  # Check the success status from the result tuple
         print("Error occurred while looking up the hero.")
         return
-    
+
+    rows = result[0]  # Fetch the rows from the result tuple
+
     if rows:
         hero_id = rows[0][0]
-        delete_query = "DELETE FROM Heroes WHERE id = %s"
+        delete_query = "DELETE FROM heroes WHERE id = %s"
         delete_params = (hero_id, )
         execute_query(delete_query, delete_params)
         print(f"Hero with ID {hero_id} deleted successfully.")
@@ -19,4 +44,4 @@ def delete_hero(hero_name):
         print(f"Hero '{hero_name}' not found.")
 
 hero_name_input = input("Type in which hero you would like to delete: ")
-execute_query(delete_hero(hero_name_input))
+delete_hero(hero_name_input)
